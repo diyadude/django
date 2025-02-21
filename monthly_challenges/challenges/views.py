23c3f4fbcc226a2f98a61c301b3f77b02a3a0ae6
Diya Qobadi <diyabedson@gmail.com>
2025-02-21 03:01:56 -0500

    feat: add full support for all months and their challenges in str mode

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month.",
+    "february": "Walk for at least 20 minutes every day.",
+    "march": "Learn Django for at least 20 minutes every day.",
+    "april": "Read 10 pages of a book before bed.",
+    "may": "Drink a full glass of water first thing in the morning.",
+    "june": "Write down one thing you're grateful for each day.",
+    "july": "Learn a new word and use it in conversation.",
+    "august": "Spend 15 minutes decluttering a space in your home.",
+    "september": "Reach out to a friend or family member just to say hi.",
+    "october": "Try a new healthy recipe for dinner.",
+    "november": "Practice mindfulness or meditate for 10 minutes.",
+    "december": "Try not to love her (impossible)."
+}
 
 
 def index(request):
     return HttpResponse("This works!")
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+
+        return HttpResponse(challenge_text)
+    except Exception:
+        return HttpResponseNotFound("Invalid input!")

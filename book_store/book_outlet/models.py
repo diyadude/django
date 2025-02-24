b8f351d77655449b5bc323fc0b53b8eb4c659bef
Diya Qobadi <diyabedson@gmail.com>
2025-02-24 16:44:55 -0500

    feat(book_store): add and register Address  model - make an one-to-one relation (author to address)

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -6,9 +6,25 @@ from django.urls import reverse
 # Create your models here.
 
 
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=55)
+    city = models.CharField(max_length=55)
+    
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+    
+    class Meta:
+        verbose_name_plural = "Adresses"
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=55)
     last_name = models.CharField(max_length=105)
+    # one-to-one relation
+    # Author.address.street
+    address = models.OneToOneField(
+        Address, on_delete=models.CASCADE, null=True)
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -22,6 +38,7 @@ class Book(models.Model):
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     # one-to-many relation
+    # Author.book_set.all - related_name > Author.books.all
     author = models.ForeignKey(
         Author, on_delete=models.CASCADE, related_name='books')
     is_best_selling = models.BooleanField(default=False)

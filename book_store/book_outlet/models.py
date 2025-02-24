3854b92ebdde32cee6a94db1ea5c77a9e7d3a24b
Diya Qobadi <diyabedson@gmail.com>
2025-02-24 16:12:20 -0500

    feat(book_store): add and register Author model - make an one-to-many relation (book to author)

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -6,21 +6,34 @@ from django.urls import reverse
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=105)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
 class Book(models.Model):
     title = models.CharField(max_length=55)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(max_length=105, null=True)
+    # one-to-many relation
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name='books')
     is_best_selling = models.BooleanField(default=False)
     slug = models.SlugField(
         default='', blank=True, db_index=True)
-    
+
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
-    
+
     # def save(self, *args, **kwargs):
     #     self.slug = slugify(self.title)
     #     super().save(*args, **kwargs)
-    
+
     def __str__(self):
         return f"{self.title}"

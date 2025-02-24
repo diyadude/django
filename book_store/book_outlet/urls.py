feb437473a31e03dc82d66a934680b503f25ecdb
Diya Qobadi <diyabedson@gmail.com>
2025-02-24 05:37:31 -0500

    feat(book_store): add book page and render data onto the pages

--- book_store/book_outlet/urls.py
+++ book_store/book_outlet/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('books/<int:pk>/', views.book_detail, name='book-detail')
 ]
--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render,
+    # get_object_or_404
+)
+from django.http import Http404
+
+from .models import Book
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'book_outlet/index.html')
+    books = Book.objects.all()
+    context = {
+        "books": books
+    }
+    return render(request, 'book_outlet/index.html', context)
+
+
+def book_detail(request, pk):
+    # book = get_object_or_404(Book, pk=pk)
+    try:
+        book = Book.objects.get(id=pk)  # or .get(pk=variable)
+    except Exception:
+        raise Http404()
+
+    context = {
+        "book": book
+    }
+    return render(request, 'book_outlet/book.html', context)

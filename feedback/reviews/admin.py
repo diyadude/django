396b913b56c5ddc0133a86290e6e98fd70d183df
Diya Qobadi <diyabedson@gmail.com>
2025-02-25 12:54:13 -0500

    feat(feedback): store the input values into our model

--- feedback/reviews/admin.py
+++ feedback/reviews/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Review
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'rating')
+
+
 # Register your models here.
+
+admin.site.register(Review, ReviewAdmin)

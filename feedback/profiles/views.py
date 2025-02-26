9136901c958690bdea22d40cefe9fb3cde8535cb
Diya Qobadi <diyabedson@gmail.com>
2025-02-26 10:50:30 -0500

    refactor(profiles): use CreateView view class

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -8,23 +9,30 @@ from .models import UserProfile
 # Create your views here.
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        context = {
-            "form": form
-        }
-        return render(request, "profiles/create-profile.html", context)
+class CreateProfileView(CreateView):
+    template_name = "profiles/create-profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
+
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         context = {
+#             "form": form
+#         }
+#         return render(request, "profiles/create-profile.html", context)
+
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
         
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES['user_image'])
-            profile.save()
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image=request.FILES['user_image'])
+#             profile.save()
 
-            return HttpResponseRedirect('/profiles')            
-        context = {
-            "form": submitted_form
-        }
-        return render(request, "profiles/create-profile.html", context)
+#             return HttpResponseRedirect('/profiles')            
+#         context = {
+#             "form": submitted_form
+#         }
+#         return render(request, "profiles/create-profile.html", context)
